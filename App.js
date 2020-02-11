67e03157ef1e18db241c344830556c890110ed2d
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-02-11 11:36:24 +0100

    Added react-navigation and dependencies to the project.

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-class App extends Component {
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  );
 }
+
+function ScoreScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Regnskab</Text>
+    </View>
+  );
+}
+
+function StatisticsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Stats!</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Regnskab" component={ScoreScreen} />
+        <Tab.Screen name="Statestik" component={StatisticsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,6 +49,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    
+  }
 });
-
-export default App;
